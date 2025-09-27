fbd0a84b1492b2839b19f43390713c12db2eea66
Dizzrt <dizzrt.s@qq.com>
2025-09-27 09:48:48 +0800

    update

--- cmd/wire.go
+++ cmd/wire.go
@@ -6,11 +6,12 @@ package cmd
 import (
 	"github.com/Dizzrt/ellie"
 	"github.com/Dizzrt/ellie-layout/internal/conf"
+	"github.com/Dizzrt/ellie-layout/internal/iface"
 	"github.com/Dizzrt/ellie-layout/internal/server"
 	"github.com/Dizzrt/ellie/log"
 	"github.com/google/wire"
 )
 
 func wireApp(bootstrap *conf.Bootstrap, logger log.LogWriter) (*ellie.App, func(), error) {
-	panic(wire.Build(newApp, server.ProviderSet))
+	panic(wire.Build(newApp, server.ProviderSet, iface.ProviderSet))
 }
